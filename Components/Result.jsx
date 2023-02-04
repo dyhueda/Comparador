3419a3de18ec21386bc0115907b6811283383e83
Diego Ueda <dyhueda@gmail.com>
2023-02-05 01:06:10 +0900

    resolvendo problema na logica

--- Components/Result.jsx
+++ Components/Result.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Result = ({ state }) => {
   const { quantidade1, quantidade2, preco1, preco2, medida1, medida2} = state;
-  const preçoitem1 = Number((state.quantidade1*state.medida1/state.preco1)).toFixed(2)
-  const preçoitem2 = Number((state.quantidade2*state.medida2/state.preco2)).toFixed(2)
+  const preçoitem1 = Number((state.preco1/state.quantidade1*state.medida1)).toFixed(2)
+  const preçoitem2 = Number((state.preco2/state.quantidade2*state.medida2)).toFixed(2)
   return (
     <div className="result">
       <div>
